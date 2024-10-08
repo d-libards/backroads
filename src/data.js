a009fc8f951d2fa0dae3bb8665a90e8d5451d13e
d-libards <dominiquelibardo09@gmail.com>
2024-10-08 20:34:42 +0800

    navbar done

---
+++ src/data.js
@@ -0,0 +1,21 @@
+export const links = [
+  {
+    link: 'https://www.twitter.com',
+    icon: 'fab fa-facebook',
+  },
+  {
+    link: 'https://www.twitter.com',
+    icon: 'fab fa-twitter',
+  },
+  {
+    link: 'https://www.twitter.com',
+    icon: 'fab fa-squarespace',
+  },
+]
+
+export const hrefs = [
+  { href: '#home', title: 'Home' },
+  { href: '#about', title: 'About' },
+  { href: '#services', title: 'Secvices' },
+  { href: '#tours', title: 'Tours' },
+]
